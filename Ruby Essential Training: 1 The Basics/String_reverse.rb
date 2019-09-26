1b922d01fa17affe11e0a611e3c6cb38f3e4bcf2
Srijita <srmukher@redhat.com>
2019-09-26 16:10:40 +0530

    Create String_reverse.rb

---
+++ Ruby Essential Training: 1 The Basics/String_reverse.rb
@@ -0,0 +1,7 @@
+puts "what's you first name?"
+first_name = gets.chomp
+puts "What's your last name?"
+last_name = gets.chomp
+full_name = first_name +" "+ last_name
+puts "Your full name is #{full_name}"
+puts "Your name when reversed is #{full_name.reverse}"
