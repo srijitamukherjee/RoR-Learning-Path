055d8c17c5836263413fe649eabaa46c8ceda541
hema <hema@localhost.localdomain>
2019-10-01 12:16:32 +0530

    Added test-unit

---
+++ Programming Foundations: Algorithms/Factorial-test/test_unit/Facttest.rb
@@ -0,0 +1,21 @@
+require "test/unit"
+require_relative "/home/hema/RoR-Learning-Path/Programming Foundations: Algorithms/Factorial-test/fact"
+
+
+
+class Facttest < Test::Unit::TestCase
+
+  def test_positive
+    assert_equal(6, Fact.new(3).fact)
+  end
+
+  def test_Zero
+    assert_equal(1, Fact.new(0).fact)
+  end
+
+  def test_negative
+  Fact.new(-5).fact
+  assert_raise do
+  end
+  end
+end
