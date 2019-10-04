c25f5e312fc93e0a84c84960d9d9fd3852d50af0
hema <hema@localhost.localdomain>
2019-10-04 18:19:30 +0530

    Implementing logic to find the power of number

--- Hema/lib/first_project/power.rb
+++ Hema/lib/first_project/power.rb
@@ -1,6 +1,20 @@
 module Hema
 
   class Power
-    
+    def self.pow(num,power)
+      result = 1.0
+      if(power >=0)
+        for i in (1..power) do
+         result = result*num
+        end
+        return result
+      else
+      power = -1*power
+        for i in (1..power) do
+          result = result*(1/num)
+        end
+        return result.round(9)
+      end
+    end
   end
 end
